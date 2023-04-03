9d2a9fcea29e68aa5b60c1f3af078f8897f7f448
hongdayeong <dudgirl135@naver.com>
2023-04-04 01:50:46 +0900

    GNB 추가

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,11 +1,20 @@
-import React from "react"
+import { Link } from "gatsby";
+import React from "react";
 
 function TestPage() {
   return (
     <main>
-        <title>테스트</title>
-        <h1>테스트 페이지입니다</h1>
-        </main>
+      <title>테스트</title>
+
+      <div style={{ display: "flex", gap: 10 }}>
+        <Link to="/">홈</Link>
+        <Link to="/about">어바웃</Link>
+        <Link to="/test">테스트</Link>
+        <Link to="/test-space">테스트</Link>
+      </div>
+
+      <h1>테스트 페이지 입니다.</h1>
+    </main>
   );
 }
 
