0ce3b0db0dc6867aa6b8f65c61b57452b24dd92f
hongdayeong <dudgirl135@naver.com>
2023-04-05 17:01:14 +0900

    사이트 제목 꾸미기

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: "my First Gatsby Site",
-    description: "이 사이트는 개발자 장희성의 개발일지가 담겨있습니다."
   },
   plugins: [
     "gatsby-plugin-image", "gatsby-plugin-sharp",],
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,14 @@
-import { Link } from "gatsby";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import React from "react";
 
 import {
-} from "./layout.module.css";
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText,
+    siteTitle,
+  } from "./layout.module.css";
 
 function Layout({ pageTitle, children }) {
   const data = useStaticQuery(graphql`
@@ -23,7 +28,7 @@ function Layout({ pageTitle, children }) {
       <title>
         {pageTitle} | {data.site.siteMetadata.title}
       </title>
-      <header>{data.site.siteMetadata.title}</header>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <div>{data.site.siteMetadata.description}</div>
       <nav>
         <ul className={navLinks}>
