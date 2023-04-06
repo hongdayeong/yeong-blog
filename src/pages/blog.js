fef3026a1673b8f295403387d37dba2c84435234
hongdayeong <dudgirl135@naver.com>
2023-04-06 15:56:11 +0900

    second 블로그 포스트 리스트

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,12 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
-import { graphql } from "gatsby";
 
 function BlogPage() {
   const data = useStaticQuery(graphql`
     query {
       allFile {
-        nodes{
+        nodes {
           name
         }
       }
@@ -15,17 +15,14 @@ function BlogPage() {
 
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in herenp</p>
+      <p>My cool posts will go in here</p>
 
       <ul>
         {data.allFile.nodes.map((node) => (
           <li key={node.name}>{node.name}</li>
         ))}
-       
       </ul>
-      
     </Layout>
   );
 }
-
 export default BlogPage;
