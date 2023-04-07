c16538d808d34181a16371be5fcedf456c1bc546
hongdayeong <dudgirl135@naver.com>
2023-04-08 01:16:15 +0900

    restart

---
+++ src/pages/blog/index.js
@@ -0,0 +1,33 @@
+import { graphql, Link } from "gatsby";
+import React from "react";
+import Layout from "../../components/layout";
+
+function BlogPage({data}) {    
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      ))}
+    </Layout>
+  );
+}
+
+export const query = graphql`
+query {
+  allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+      }
+      id
+      slug
+    }
+  }
+}
+`;
+
+export default BlogPage;
